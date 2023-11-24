d0366e595551a4ac347f3a588487dbd3cfe24bb9
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-11-23 21:43:17 -0500

    Adding programming principles

---
+++ programming_fundamentals.js
@@ -0,0 +1,39 @@
+// Read this article on programming principles, focus on the following nine:
+
+// DRY
+// KISS
+// Avoid creating a YAGNI
+// Do the simplest thing that could possibly work
+// Don't make me think
+// Write code for the maintainer
+// Single responsibility principle
+// Avoid premature optimization
+// Separation of concerns
+
+// Write a ~1 sentence summary for each one
+// DRY: Don't write a piece of code again and again, that is, avoid repetition. Instead use constructs like loops, functions etc.
+
+//KISS: Keep the code simple and readable. It is easier to write and debug.
+
+//Avoid creating a YAGNI: Only write code for the functionality you need, nothing extra.
+
+//Do the simplest thing that could possibly work: Don't try to write complex work. try to write a simple code that will do the work. This makes it easy to debug and modify.
+
+//Don't make me think: Write simple and readable code so that other people can understand what the code is doing with minimal effort.
+
+// Write code for the maintainer: Add comments and write the code in such a way that in future you or someone new maintaining the code will not have difficulty understanding it.
+
+//Single responsibility principle: A part of code such as a function or class should do only one single task. Create separate code components for separate tasks.
+
+//Avoid premature optimization: First ensure the code is working as expected before trying to go into optimization. Once code is working (maybe be slow) then focus on optimizing it.
+
+//Separation of concerns: Divide the code into different sections with each section pertaining to a separate functionality.
+
+
+// Which ones surprise you (if any)?
+//Avoid premature optimization principle was a surprise because I always assumed it was important not just to make the code work but make it work in an optimized way.
+
+// Which one is currently giving you the most struggle?
+//Write code for maintainer is difficult to implement because when I am writing the code it doesn't come naturally to me that other people looking at it might not understand. Because when I am writing the code I have been thinking about it for a long time but when someone new is looking at it for the first time, the code might not be easy to understand at first.
+
+
