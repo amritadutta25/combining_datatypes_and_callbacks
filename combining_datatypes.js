5ce448783f4a3e78f919c7af1ae30e05ef088c17
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-11-24 00:40:11 -0500

    Combine objects, arrays, and functions more than one level deep

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -1,3 +1,7 @@
+//=================================================================
+//Combine objects, arrays, and functions more than one level deep
+//=================================================================
+
 //1. Create an object, called crayonBox, that has a property that is an array. The array should have the names of some crayons. Log one of the elements of that array.
 
 const crayonBox = {
@@ -23,6 +27,7 @@ const receipt = [
     {name: 'shoes', price:65}
 ]
 console.log(receipt[0].name)
+console.log(receipt[0].name, receipt[0].price)
 
 //4. Create an array called apartmentBuilding that has an array as one of its elements, the inner array should be the names of the tenants. Log one of the elements of the inner array.
 
@@ -33,7 +38,45 @@ const apartmentBuilding = [
 
 console.log(apartmentBuilding[0][0])
 
+//============================================
+//Combine objects, arrays, and functions more than one level deep
+//============================================
 
+//1. Create a function knit that returns an object that has the following kinds of properties item: scarf, size : 6ft etc. Log a value of that object (hint: call the function and then call a property on the return value).
 
+function knit() 
+{
+    const obj = {
+        item: 'scarf',
+        size: '6ft'
+    }
+    return obj
+}
 
+console.log(knit()['item'])  // knit() returns the object obj. Then calling the property 'item' on the returned object.
+
+//2. Create a function crayonSelector that returns an object that has an array (you can reuse your crayonBox object). Log one of the elements of that array.
+
+function crayonSelector()
+{
+    const crayonBox = {
+        names: ['blue', 'black', 'green']
+    }
+    return crayonBox
+}
+
+console.log(crayonSelector()['names'][1]) // calling the function crayonSelector() returns the crayonBox object. On this returned crayonBox object, accessing its property 'names' and then getting the second value of the array.
+
+//3. Create a function powerButton that returns a function called options - options should console.log a simple message like select a song. Call that inner function
+
+function powerButton()
+{
+    const options = () => {
+        console.log("Select a song")
+    }
+    return options
+}
+
+const optionsFunc = powerButton() //calls the function powerButton that returns the function 'options'
+optionsFunc()  // invoke the function options
 
